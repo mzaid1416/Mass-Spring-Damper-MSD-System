d73b332ff697b29b01d3bc84a131cde7f56041f1
Muhammad Zaid <muhammad14zaidi12@gmail.com>
2026-07-14 03:45:03 -0700

    Add files via upload
    MATLAB and simulink Files

---
+++ Model 1 MATLAB file.m
@@ -0,0 +1,5 @@
+% Project Quantities
+
+m = 300    % Mass - Kilograms
+b = 30     % Damping  
+c = 100    % Spring constant
---
+++ Model 2 MATLAB file.m
@@ -0,0 +1,5 @@
+% Project Quantities
+
+m = 300       % Mass - Kilograms
+b = 300        % Damping  
+c = 10000     % Spring constant
